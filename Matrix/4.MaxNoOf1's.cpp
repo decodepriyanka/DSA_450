e838d9c73f76986a035049f59d19a623ef283d35
Priyanka Kumari <64683009+decodepriyanka@users.noreply.github.com>
2023-07-22 12:26:25 +0530

    4.MaxNoOf1's.cpp

--- Matrix/4.MaxNoOf1's.cpp
+++ Matrix/4.MaxNoOf1's.cpp
@@ -44,3 +44,28 @@ int main()
 
     return 0;
 }
+---------------------- Approach 2 using Binary Search ------------------------------------------------------
+TC : O(NlogM) , SC: (1)
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	  int index = INT_MAX;
+	  int row = -1;
+	  for(int i =0;i<n;i++){
+	      int low = 0;
+	      int high = m-1;
+	      int mid ; 
+	      while(low<=high){
+	          mid = (low+high)/2;
+	          if(arr[i][mid]==1){
+	              if(index>mid){
+	                  index=mid;
+	                  row = i;
+	              }
+	              high = mid-1;
+	          }
+	          else low = mid+1;
+	         
+	      }
+	  }
+	  return row;
+	}
